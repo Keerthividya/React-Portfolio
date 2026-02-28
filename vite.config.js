50b43a2b6774f8aaa82a3bd55f0cc9f70a73a498
Vidyakeerthi SU <vidyakeerthi@payorch.com>
2026-02-28 20:58:22 +0530

    Update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/React-Portfolio/',
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
-                        if (id.includes('swiper'))
-                            return 'swiper';
-                        return;
-                    }
-                }
-            }
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    },
+  base: '/React-Portfolio/',
+  plugins: [vue()],
 })
