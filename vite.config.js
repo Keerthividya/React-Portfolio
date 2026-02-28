2d89190aa821793c63ffacc77d484618b90bf1b0
Vidyakeerthi SU <vidyakeerthi@payorch.com>
2026-02-28 20:49:41 +0530

    Update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/React-Portfolio/',
-    plugins: [vue()],
+    plugins: [react()],
     build: {
         rollupOptions: {
             output: {
